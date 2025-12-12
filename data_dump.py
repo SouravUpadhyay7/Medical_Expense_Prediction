f16bfaea758e1ea47b3d6e1494175e63a39bfa7e
sourav upadhyay <pkrsourav@gmail.com>
2025-12-13 00:14:30 +0530

    Data Dump and MongoDB Connection Done also added the Dataset and made the folder structure using Python Script

---
+++ data_dump.py
@@ -0,0 +1,35 @@
+import pymongo # pip install pymongo
+import pandas as pd
+import json
+
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://souravupadhyaybit_db_user:sourav1234@cluster0.bb3ajn6.mongodb.net/?appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+DATA_FILE_PATH = (r"C:\Users\Sourav\Desktop\ML project\insurance.csv")
+DATABASE_NAME = "INSURANCE"
+COLLECTION_NAME = "Medical_Expense_Project"
+
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    df.reset_index(drop = True, inplace = True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
